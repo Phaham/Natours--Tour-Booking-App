3cfc207ee5a4b409551c3923aeff7b35a0eb55eb
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2023-07-12 01:41:58 +0530

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,9 +6,10 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  // console.log(value);
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  // // console.log(value);
+  // const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value.`;
   return new AppError(message, 400);
 };
 
