5611ea5117b6690fa1c5dcc3c7cdc9b6eded3c9c
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2024-11-11 15:54:20 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
   const url = `${req.protocol}://${req.get("host")}/me`;
-  await new Email(newUser, url).sendWelcome();
+  // await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, req, res);
 });
 
